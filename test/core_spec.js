54ca3d0258019d3470317852497160ca205fb7fa
Yohahn Kim <yohahn.kim@gmail.com>
2015-11-12 23:23:25 -0500

    Add vote()

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 
@@ -43,6 +43,52 @@ describe('application logic', () => {
       }));
     });
 
+  });
+
+  describe('vote', () => {
+
+    it('creates a tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later')
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Transpotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later'),
+          tally: Map({
+            'Transpotting': 1
+          })
+        }),
+        entries: List()
+      }));
+    });
+
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later'),
+          tally: Map({
+            'Transpotting': 3,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Transpotting');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Transpotting', '28 Days Later'),
+          tally: Map({
+            'Transpotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List()
+      }));
+    });
   });
 
 });
